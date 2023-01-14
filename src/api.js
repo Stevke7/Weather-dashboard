ea04b621919af3e8d10624c850ddd2438affbc4e
Stevanovic <stevanovicljubisa07@gmail.com>
2023-01-15 00:36:51 +0100

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "28a6b312e6mshefd2dd252115febp1f090cjsn0e76d3b87f18",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d1a4807b977d433bc37b6ccfbcf5015c";
